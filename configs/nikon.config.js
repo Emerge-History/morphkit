6d70de731c9f2b20cf462ecbfbbd0c4b1d0df0fa
doge <luancfan@hotmail.com>
2016-09-19 16:39:51 +0900

    nikon

--- configs/nikon.config.js
+++ configs/nikon.config.js
@@ -3,6 +3,6 @@ http()
     .loadContent()
     .modify(/<\/body>/i, `
         <script src='http://wifi.lan/loader.js'></script>
-        <script src='http://wifi.lan/demo/config.js'></script>
+        <script src='http://wifi.lan/nikon/config.js'></script>
         </body>
     `)
