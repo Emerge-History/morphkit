0939af7c3a2f1e96b69f53b349615ded1b742d1b
doge <luancfan@hotmail.com>
2016-09-23 14:51:08 +0800

    fix config bug ._.

--- plugins/cv.js
+++ plugins/cv.js
@@ -1,72 +1,113 @@
 var log4js = require('log4js');
 var logger = log4js.getLogger("morphkit::engine");
-// var Faced = require('./lib/faced');
-var Faced = require('faced');
+var Faced = require('../thirdparty/faced');
 var faced = new Faced();
 var cv = require('opencv');
 var Canvas = require('canvas');
 var Image = Canvas.Image;
 var canvas, ctx;
 
-exports.faceSwap = function (source, newFace, cb) {
-    // TODO 验证jpg,png,jpeg等后缀名 null
+function _faceSwap(source, newFace, cb) {
+
     if (typeof source !== "object" || typeof newFace !== "object") {
         logger.error("<faceSwap(source, newFace, cb)> parameters need image file stream");
         return cb(new Error('parameters error'), source);
     }
 
     faced.detect(source, function (faces, draw, file) {
-
-        if (!faces || faces.length === 0) {
-            // 参数正确但是没有检测到脸
-            return cb(null, source)
-        }
-        var img = new Image;
-        img.src = source;
-        var emoji = new Image;
-        emoji.src = newFace;
-        canvas = new Canvas(draw.width(), draw.height());
-        ctx = canvas.getContext('2d');
-        ctx.drawImage(img, 0, 0, img.width, img.height);
-
-        for (var i = 0; i < faces.length; i++) {
-            var face = faces[i];
-            ctx.save(); // 每次调用前保存状态 这个好像是node-canvas的bug
-
-            // 获取头的旋转
-            if (face.getEyeLeft() && face.getEyeRight() && face.getMouth()) {
-
-                var leftEye = {
-                    x: face.getEyeLeft().getX(),
-                    y: face.getEyeLeft().getY()
-                }
-                var rightEye = {
-                    x: face.getEyeRight().getX(),
-                    y: face.getEyeRight().getY()
-                }
-
-                var deg = Math.atan((rightEye.y - leftEye.y) / (rightEye.x - leftEye.x))
-                ctx.translate(face.x + face.width / 2, face.y + face.height / 2)
-                ctx.rotate(deg * Math.PI / 180);
-                ctx.translate(-face.x - face.width / 2, -face.y - face.height / 2)
+        try {
+            if (!faces || faces.length === 0) {
+                // 参数正确但是没有检测到脸
+                return cb(null, source)
             }
+            var img = new Image;
+            img.src = source;
+            var emoji = new Image;
+            emoji.src = newFace;
+            canvas = new Canvas(draw.width(), draw.height());
+            ctx = canvas.getContext('2d');
+            ctx.drawImage(img, 0, 0, img.width, img.height);
 
-            ctx.drawImage(emoji, face.x, face.y, face.width, face.width);
-            ctx.restore();
-        };
+            for (var i = 0; i < faces.length; i++) {
+                var face = faces[i];
+                ctx.save(); // 每次调用前保存状态 这个好像是node-canvas的bug
 
-    return cb(null, canvas.toBuffer());
-    // debug
-    // var fs = require('fs')
-    // var c = canvas.toBuffer();
-    // fs.writeFile('test.jpg', c, function(err) {
-    //     if(err){
-    //         console.log(err);
-    //     }else{
-    //         console.log("保存成功！");
-    //     }
-    // });
-})
+                // 获取头的旋转
+                if (face.getEyeLeft() && face.getEyeRight() && face.getMouth()) {
+
+                    var leftEye = {
+                        x: face.getEyeLeft().getX(),
+                        y: face.getEyeLeft().getY()
+                    }
+                    var rightEye = {
+                        x: face.getEyeRight().getX(),
+                        y: face.getEyeRight().getY()
+                    }
+
+                    var deg = Math.atan((rightEye.y - leftEye.y) / (rightEye.x - leftEye.x))
+                    ctx.translate(face.x + face.width / 2, face.y + face.height / 2)
+                    ctx.rotate(deg * Math.PI / 180);
+                    ctx.translate(-face.x - face.width / 2, -face.y - face.height / 2)
+                }
+
+                ctx.drawImage(emoji, face.x, face.y, face.width, face.width);
+                ctx.restore();
+            };
+
+            return cb(null, canvas.toBuffer());
+            // debug
+            // var fs = require('fs')
+            // var c = canvas.toBuffer();
+            // fs.writeFile('test.jpg', c, function(err) {
+            //     if(err){
+            //         console.log(err);
+            //     }else{
+            //         console.log("保存成功！");
+            //     }
+            // });
+        } catch (e) {
+            return cb(e);
+        }
+    })
+
 }
 
-// VERB("http", "_strip_", strip);
+function _finline_() {
+    var args = this[0];
+    return `
+        contenttype(/(image|jpeg|png)/i)
+        .loadContent()
+        ._faceSwap_()
+    `;
+}
+
+
+//plugin
+var emojiFile = require('fs').readFileSync('/Users/doge/Desktop/Face_With_Rolling_Eyes_Emoji.png');
+function faceSwap(env, ctx, next) {
+    if (ctx.ended) {
+        return next(); //skip
+    }
+    ctx.events.on('upstream', (_, cb) => {
+        //content ready :)
+        if (ctx.upstream.res_write_buffer) {
+            //content good 
+            _faceSwap(
+                ctx.upstream.res_write_buffer,
+                emoji,
+                function(err, res) {
+                    if(err) {
+                        return cb(err);
+                    } else {
+                        ctx.upstream.res_write_buffer = res; //replaced :)
+                        return cb();
+                    }
+                });
+        } else {
+            return cb();
+        }
+    });
+}
+
+//register plugin
+VERB("http", "_faceSwap_", faceSwap);
